843397d621b3cbedcff25046cf5d98b881962007
olegd <o.dolgarev@gmail.com>
2021-04-16 22:20:42 +0300

    feat: improve avatars

--- imports/ui/UserAvatar.jsx
+++ imports/ui/UserAvatar.jsx
@@ -1,12 +1,12 @@
-import { Meteor } from 'meteor/meteor'
-import { useTracker } from 'meteor/react-meteor-data'
-
-import React from 'react'
+import gravatarUrl from 'gravatar-url'
+import React, { useMemo } from 'react'
 
 import Avatar from '@material-ui/core/Avatar'
 import { makeStyles } from '@material-ui/core/styles'
 
 import deepOrange from '@material-ui/core/colors/deepOrange'
+
+import { useCurrentUserEmail } from './hooks/hooks.js'
 
 const useStyles = makeStyles((theme) => ({
   orange: {
@@ -15,15 +15,28 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
+const GRAVATAR_OPTS = { default: 404 }
+
 function UserAvatar () {
   const classes = useStyles()
 
-  const currUserEmail = useTracker(() => {
-    const user = Meteor.user({ emails: 1 })
-    return user?.emails?.[0].address ?? ''
-  }, [])
+  const userEmail = useCurrentUserEmail()
+  const letterAvatar = userEmail.slice(0, 1).toUpperCase()
 
-  return <Avatar className={classes.orange}>{currUserEmail.slice(0, 2).toUpperCase()}</Avatar>
+  const avatarUrl = useMemo(() => {
+    return typeof userEmail === 'string' && userEmail.length > 0
+      ? gravatarUrl(userEmail, GRAVATAR_OPTS)
+      : null
+  }, [userEmail])
+
+  return (
+    <Avatar
+      {...{ ...(typeof avatarUrl === 'string' && { src: avatarUrl }) }}
+      className={classes.orange}
+    >
+      {letterAvatar}
+    </Avatar>
+  )
 }
 
 export default UserAvatar
--- imports/ui/hooks/hooks.js
+++ imports/ui/hooks/hooks.js
@@ -1,8 +1,13 @@
-import Meteor from 'meteor/meteor'
+import { Meteor } from 'meteor/meteor'
 import { useTracker } from 'meteor/react-meteor-data'
 import React, { useContext } from 'react'
 
 const LangContext = React.createContext()
 export const useLangContext = () => useContext(LangContext)
 
-export const useCurrentUserId = useTracker(() => Meteor.userId(), [])
+export const useCurrentUserId = () => useTracker(() => Meteor.userId(), [])
+
+export const useCurrentUserEmail = () => useTracker(() => {
+  const user = Meteor.user({ emails: 1 })
+  return user?.emails?.[0].address ?? ''
+}, [])
