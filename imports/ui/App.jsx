8b03b8b2fa49dd410341574bbd19e8ce71cfb361
OlegD <o.dolgarev@gmail.com>
2021-04-18 15:16:24 +0300

    feat: imporve app localization

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,27 +5,41 @@ import { SnackbarProvider } from 'notistack'
 
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Slide from '@material-ui/core/Slide'
-import AppBody from './AppBody.jsx'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
+import { enUS, ruRU } from '@material-ui/core/locale'
+
+import AppBody from './AppBody.jsx'
 import AppState from './AppState.js'
+
+const LOCALES = {
+  en: enUS,
+  ru: ruRU
+}
+const DEFAULT_LOCALE = 'enUS'
 
 function App () {
   const lang = useTracker(() => AppState.get('app.lang'), [])
   i18n.setLocale(lang, { async: true, fresh: true })
 
+  const locale = LOCALES[lang] ?? DEFAULT_LOCALE
+  const theme = createMuiTheme({}, locale)
+
   return (
-    <SnackbarProvider
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'center'
-      }}
-      TransitionComponent={Slide}
-      preventDuplicate
-      maxSnack={3}
-    >
-      <CssBaseline />
-      <AppBody />
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        TransitionComponent={Slide}
+        preventDuplicate
+        maxSnack={3}
+      >
+        <CssBaseline />
+        <AppBody />
+      </SnackbarProvider>
+    </ThemeProvider>
   )
 }
 
